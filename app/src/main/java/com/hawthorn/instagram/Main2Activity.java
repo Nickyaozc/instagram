5ed46db3982097aa955aed114af3ef37c3895da2
yaozican <zican.yao@gmail.com>
2018-09-24 00:06:23 +1000

    fix bottomNavigationBar state problem

--- app/src/main/java/com/hawthorn/instagram/Main2Activity.java
+++ app/src/main/java/com/hawthorn/instagram/Main2Activity.java
@@ -1,13 +1,16 @@
 package com.hawthorn.instagram;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.hawthorn.instagram.Activity.ActivityFragment;
@@ -18,64 +21,33 @@ import com.hawthorn.instagram.Profile.ProfileFragment;
 
 public class Main2Activity extends AppCompatActivity {
 
-    int currentItemId;
+    //const
+    static final String TAG = "PhotoActivity";
+
+    //var
+
+    //widget
+    private FrameLayout mainFrameLayout;
+    private BottomNavigationView bottomNavigationView;
+    private HomeFragment homeFragment;
+    private DiscoveryFragment discoveryFragment;
+    private ActivityFragment activityFragment;
+    private ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        /*Initiate the bottomNavigationBar*/
-        final BottomNavigationView bottomNavigationView
-                = (BottomNavigationView) findViewById(R.id.bottom_navigation_bar);
-
-        /*Initiate the FrameLayout which holding the five different Fragments
-          corresponding to the 5 buttons in the bottomNavigationBar*/
-        final FrameLayout mainFrameLayout = (FrameLayout) findViewById(R.id.container);
-        final HomeFragment homeFragment = new HomeFragment();
-        final DiscoveryFragment discoveryFragment = new DiscoveryFragment();
-        final ActivityFragment activityFragment = new ActivityFragment();
-        final ProfileFragment profileFragment = new ProfileFragment();
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_bar);
+        mainFrameLayout = (FrameLayout) findViewById(R.id.container);
+        homeFragment = new HomeFragment();
+        discoveryFragment = new DiscoveryFragment();
+        activityFragment = new ActivityFragment();
+        profileFragment = new ProfileFragment();
 
         setFragment(homeFragment);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-                if (menuItem.getItemId() != R.id.nav_photo) {
-
-                }
-
-                switch (menuItem.getItemId()) {
-                    case R.id.nav_home:
-                        setFragment(homeFragment);
-                        return true;
-
-                    case R.id.nav_discovery:
-                        setFragment(discoveryFragment);
-                        return true;
-
-                    case R.id.nav_photo:
-                        showPhotoActivity();
-                        return true;
-
-                    case R.id.nav_activity:
-                        setFragment(activityFragment);
-                        return true;
-
-                    case R.id.nav_profile:
-                        setFragment(profileFragment);
-                        return true;
-
-                    default:
-                        return false;
-                }
-            }
-        });
-
-
+        setUpBottomNavigationListener();
     }
 
     private void setFragment(Fragment fragment) {
@@ -87,5 +59,39 @@ public class Main2Activity extends AppCompatActivity {
     public void showPhotoActivity() {
         Intent intent = new Intent(this, PhotoActivity.class);
         startActivity(intent);
+    }
+
+    private void setUpBottomNavigationListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            setFragment(homeFragment);
+                            return true;
+
+                        case R.id.nav_discovery:
+                            setFragment(discoveryFragment);
+                            return true;
+
+                        case R.id.nav_photo:
+                            showPhotoActivity();
+                            return false;
+
+                        case R.id.nav_activity:
+                            setFragment(activityFragment);
+                            return true;
+
+                        case R.id.nav_profile:
+                            setFragment(profileFragment);
+                            return true;
+
+                        default:
+                            return false;
+                    }
+                }
+            });
     }
 }
--- app/src/main/java/com/hawthorn/instagram/Photo/PhotoActivity.java
+++ app/src/main/java/com/hawthorn/instagram/Photo/PhotoActivity.java
@@ -9,8 +9,10 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.hawthorn.instagram.Main2Activity;
 import com.hawthorn.instagram.R;
 
 public class PhotoActivity extends FragmentActivity {
@@ -24,6 +26,7 @@ public class PhotoActivity extends FragmentActivity {
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
     private TextView tbCenterTextView;
+    private TextView tbCancelTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +34,15 @@ public class PhotoActivity extends FragmentActivity {
         setContentView(R.layout.activity_photo);
         setupViewPaper();
         tbCenterTextView = (TextView) findViewById(R.id.toolbarCenterTextView);
+        tbCancelTextView = (TextView) findViewById(R.id.toolbarCancelTextView);
         Log.d(TAG, "onCreate: Started");
         setupPageChangeListener();
-//        dispatchTakePictureIntent();
     }
 
-    @Override
-    public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
+
+
+    public void closePhotoActivity(View view) {
+        super.onBackPressed();
     }
 
     /**
@@ -108,87 +105,4 @@ public class PhotoActivity extends FragmentActivity {
         });
     }
 
-//    private void dispatchTakePictureIntent() {
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//
-//        /*Ensure that there's a camera activity to handle the intent:
-//        the startActivityForResult() method is protected by a condition
-//        that calls resolveActivity(), which returns the first activity component
-//        that can handle the intent.*/
-//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//            // Create the File where the photo should go
-//            File photoFile = null;
-//            try {
-//                photoFile = createImageFile();
-//            } catch (IOException ex) {
-//                ex.printStackTrace();
-//            }
-//            // Continue only if the File was successfully created
-//            if (photoFile != null) {
-//                Uri photoURI = FileProvider.getUriForFile(this,
-//                        "com.hawthorn.instagram.fileprovider",
-//                        photoFile);
-//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//            }
-//        }
-//    }
-//
-//    private File createImageFile() throws IOException {
-//        // Create an image file name
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        System.out.println("@@@@@@@@@@@" + storageDir);
-//        File image = File.createTempFile(
-//                imageFileName,  /* prefix */
-//                ".jpg",     /* suffix */
-//                storageDir      /* directory */
-//        );
-//        // Save a file: path for use with ACTION_VIEW intents
-//        mCurrentPhotoPath = image.getAbsolutePath();
-//        return image;
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode,
-//                                    Intent data) {
-//        if (requestCode == REQUEST_IMAGE_CAPTURE) {
-//            ImageView imageView = (ImageView) findViewById(R.id.myImageView);
-//            Glide.with(this).load(mCurrentPhotoPath).into(imageView);
-//        }
-//    }
-//
-//    private boolean checkPermission() {
-//        int permissionCheck = ContextCompat.checkSelfPermission(PhotoActivity.this,
-//                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-//        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-//            return false;
-//        }else{
-//            return true;
-//        }
-//    }
-//
-//    private void requestPermission() {
-//        if (ActivityCompat.shouldShowRequestPermissionRationale(PhotoActivity.this,
-//                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-//            Toast.makeText(PhotoActivity.this,
-//                    "Write External Storage permission allows us to do store images. "
-//                            + "Please allow this permission in App Settings.",
-//                    Toast.LENGTH_LONG).show();
-//        } else {
-//            ActivityCompat.requestPermissions(PhotoActivity.this,
-//                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
-//                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-//        }
-//    }
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//            Log.v("value","Permission: "+permissions[0]+ " was "+grantResults[0]);
-//            //resume tasks needing this permission
-//        }
-//    }
 }
