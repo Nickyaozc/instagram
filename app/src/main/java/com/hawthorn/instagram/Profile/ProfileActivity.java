b1dc85ec0e28638ba399d5bf8c43fbbe616db3ec
Mingchi Zhang <hlsnw2006@163.com>
2018-10-16 22:57:23 +1100

    Photo grid not done yet.

--- app/src/main/java/com/hawthorn/instagram/Profile/ProfileActivity.java
+++ app/src/main/java/com/hawthorn/instagram/Profile/ProfileActivity.java
@@ -11,14 +11,18 @@ import android.view.Menu;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 //import android.widget.Toolbar;
 
+import com.hawthorn.instagram.Utils.GridImageAdapter;
 import com.nostra13.universalimageloader.core.ImageLoader;
 //import com.android.volley.toolbox.ImageLoader;
 import com.hawthorn.instagram.R;
 import com.hawthorn.instagram.Utils.UniversalImageLoader;
+
+import java.util.ArrayList;
 //import android.support.v7.R;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -26,6 +30,8 @@ public class ProfileActivity extends AppCompatActivity {
     private ImageView profilePhoto;
     private ProgressBar mProgressBar;
     private Context mContext = ProfileActivity.this;
+    private static final int ACTIVITY_NUM = 4;
+    private static final int NUM_GRID_COLUMNS = 3;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,6 +44,35 @@ public class ProfileActivity extends AppCompatActivity {
 //        mProgressBar.setVisibility(View.GONE);
         setupActivityWidgets();
         setProfileImage();
+        tempGridSetup();
+    }
+
+
+    private void tempGridSetup(){
+        ArrayList<String> imgURLs = new ArrayList<>();
+        imgURLs.add("https://pbs.twimg.com/profile_images/616076655547682816/6gMRtQyY.jpg");
+        imgURLs.add("https://i.redd.it/9bf67ygj710z.jpg");
+        imgURLs.add("https://c1.staticflickr.com/5/4276/34102458063_7be616b993_o.jpg");
+        imgURLs.add("http://i.imgur.com/EwZRpvQ.jpg");
+        imgURLs.add("http://i.imgur.com/JTb2pXP.jpg");
+        imgURLs.add("https://i.redd.it/59kjlxxf720z.jpg");
+        imgURLs.add("https://i.redd.it/pwduhknig00z.jpg");
+        imgURLs.add("https://i.redd.it/clusqsm4oxzy.jpg");
+        imgURLs.add("https://i.redd.it/svqvn7xs420z.jpg");
+        imgURLs.add("http://i.imgur.com/j4AfH6P.jpg");
+        imgURLs.add("https://i.redd.it/89cjkojkl10z.jpg");
+        imgURLs.add("https://i.redd.it/aw7pv8jq4zzy.jpg");
+
+        setupImageGrid(imgURLs);
+    }
+
+    private void setupImageGrid(ArrayList<String> imgURLs){
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+        int imageWidth = gridWidth/NUM_GRID_COLUMNS;
+        gridView.setColumnWidth(imageWidth);
+        GridImageAdapter adapter = new GridImageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
+        gridView.setAdapter(adapter);
     }
 
     private void initImageLoader() {
@@ -56,12 +91,9 @@ public class ProfileActivity extends AppCompatActivity {
         mProgressBar.setVisibility(View.GONE);
         profilePhoto = (ImageView) findViewById(R.id.profile_photo);
     }
-
-
     private void setupToolbar(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
         setSupportActionBar(toolbar);
-
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
@@ -70,10 +102,7 @@ public class ProfileActivity extends AppCompatActivity {
                     case R.id.profileMenu:
                         Log.d(TAG, "onMenuItemClick: navigate to user profile.");
                 }
-
                 return false;
-
-
             }
         });
         ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
