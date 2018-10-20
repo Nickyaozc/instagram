67506d7c9d339d60671bc232259c44858dfe6a66
weiren chen <weirenchen@weirens-MBP.gateway>
2018-10-20 15:12:35 +1100

    discovery

--- app/src/main/java/com/hawthorn/instagram/Discovery/DiscoveryFragment.java
+++ app/src/main/java/com/hawthorn/instagram/Discovery/DiscoveryFragment.java
@@ -1,31 +1,156 @@
 package com.hawthorn.instagram.Discovery;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.util.Log;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 import com.hawthorn.instagram.R;
+import com.hawthorn.instagram.Utils.UserListAdapter;
+import com.hawthorn.instagram.models.Users;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
+import static android.support.v4.content.ContextCompat.getSystemService;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DiscoveryFragment extends Fragment {
+    private static final String TAG = "discoveryfragment";
+    private static final int ACTIVITY_NUM = 1;
+
+
+    private Context mContext = getContext();
+
+    //widgets
+    private EditText mSearchParam;
+    private ListView mListView;
 
 
     public DiscoveryFragment() {
         // Required empty public constructor
     }
+    //vars
+    private List<Users> mUserList;
+    private UserListAdapter mAdapter;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discovery, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_discovery, container, false);
+        mSearchParam = view.findViewById(R.id.search);
+        mListView = view.findViewById(R.id.listView);
+        initTextListener();//
+
+
+
+        return view;
     }
+
+
+    private void initTextListener(){
+        Log.d(TAG, "initTextListener: initializing");
+
+        mUserList = new ArrayList<>();
+
+        mSearchParam.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                String text = mSearchParam.getText().toString().toLowerCase(Locale.getDefault());
+                searchForMatch(text);
+            }
+        });
+    }
+
+    private void searchForMatch(String keyword){
+        Log.d(TAG, "searchForMatch: searching for a match: " + keyword);
+        mUserList.clear();
+        //update the users list view
+        if(keyword.length() ==0){
+
+        }else{
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            Query query = reference.child(getString(R.string.dbname_users))
+                    .orderByChild(getString(R.string.field_username)).equalTo(keyword);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+                        Log.d(TAG, "onDataChange: found user:" + singleSnapshot.getValue(Users.class).toString());
+
+                        mUserList.add(singleSnapshot.getValue(Users.class));
+                        //update the users list view
+                        updateUsersList();
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    private void updateUsersList(){
+        Log.d(TAG, "updateUsersList: updating users list");
+
+        mAdapter = new UserListAdapter(getContext(), R.layout.layout_user_listitem, mUserList);
+
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected user: " + mUserList.get(position).toString());
+
+                //navigate to profile activity
+
+            }
+        });
+    }
+
+
+
+
+
 
 }
---
+++ app/src/main/java/com/hawthorn/instagram/models/UserAccountSettings.java
@@ -0,0 +1,89 @@
+package com.hawthorn.instagram.models;
+
+import java.io.Serializable;
+
+public class UserAccountSettings {
+    public String description;
+    public String display_name;
+    public long followers;
+    public long following;
+    public long posts;
+    public String profile_photo;
+    public String username;
+    public String website;
+
+    public UserAccountSettings(String description, String display_name, long followers, long following, long posts, String profile_photo, String username, String website) {
+        this.description = description;
+        this.display_name = display_name;
+        this.followers = followers;
+        this.following = following;
+        this.posts = posts;
+        this.profile_photo = profile_photo;
+        this.username = username;
+        this.website = website;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDisplay_name() {
+        return display_name;
+    }
+
+    public void setDisplay_name(String display_name) {
+        this.display_name = display_name;
+    }
+
+    public long getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(long followers) {
+        this.followers = followers;
+    }
+
+    public long getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(long following) {
+        this.following = following;
+    }
+
+    public long getPosts() {
+        return posts;
+    }
+
+    public void setPosts(long posts) {
+        this.posts = posts;
+    }
+
+    public String getProfile_photo() {
+        return profile_photo;
+    }
+
+    public void setProfile_photo(String profile_photo) {
+        this.profile_photo = profile_photo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+}
