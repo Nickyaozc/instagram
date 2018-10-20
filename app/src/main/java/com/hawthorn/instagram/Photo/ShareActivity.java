ccd7b8aa2e42f00c8fee23be54d772660094733e
yaozican <zican.yao@gmail.com>
2018-10-20 15:34:58 +1100

    add photo models

--- app/src/main/java/com/hawthorn/instagram/Photo/ShareActivity.java
+++ app/src/main/java/com/hawthorn/instagram/Photo/ShareActivity.java
@@ -8,7 +8,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.hawthorn.instagram.R;
+import com.hawthorn.instagram.Utils.FirebaseMethods;
 
 public class ShareActivity extends AppCompatActivity {
 
@@ -16,6 +20,13 @@ public class ShareActivity extends AppCompatActivity {
 
     //widgets
     private ImageView mImageView;
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseMethods mFirebaseMethods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
